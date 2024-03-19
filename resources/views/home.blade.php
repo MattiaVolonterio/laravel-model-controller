95a2f24b3c9f363003cb4490bc7888e5a8440820
MattiaVolonterio <volonteriomattia@gmail.com>
2024-03-19 15:35:22 +0100

    feat: basic layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('template.app')
 
-  <head>
-    <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="ie=edge" http-equiv="X-UA-Compatible">
-    <title>Document</title>
+@section('page_title', 'Home')
 
-    @vite('resources/js/app.js')
-  </head>
-
-  <body>
-    <h1>Laravel with Bootstrap + Vite</h1>
-  </body>
-
-</html>
+@section('main')
+    <div class="container mt-3">
+        <h1>Laravel with Bootstrap + Vite</h1>
+    </div>
+@endsection
